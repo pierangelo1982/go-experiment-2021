9a05b00a1550f3837544e17ce41160ba9a03f0c3
pierangelo1982 <pierangelo1982@gmail.com>
2021-08-01 11:09:23 +0200

    goodbye

--- 02_httpServer/02_httpServer/main.go
+++ 02_httpServer/02_httpServer/main.go
@@ -11,9 +11,14 @@ func main() {
 	l := log.New(os.Stdout, "products-api ", log.LstdFlags)
 
 	hh := handlers.NewHello(l)
+	hg := handlers.NewGoodbye(l)
 
 	sm := http.NewServeMux()
 	sm.Handle("/", hh)
+	sm.Handle("/goodbye", hg)
 
 	http.ListenAndServe(":8080", sm)
 }
+
+// curl -v -d"pierangelo" localhost:8080
+// curl -v localhost:8080/goodbye
